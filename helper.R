0e41baa7209a31d514de601824c84dd8b72fdcd4
David Ritter <djr521@gmail.com>
2020-02-18 08:42:53 -0500

    filtering

--- helper.R
+++ helper.R
@@ -52,7 +52,6 @@ tidy_choose_all <- function(data)
   }
 
 # Clean question data for plotting
-
 clean_choose_all <- function(data)
 {
   q_data %>%
@@ -74,7 +73,7 @@ plot_choose_all <- function(data)
     ylab("% Responded, Count")
 }
 
-
+# Clean signle select data for plotting
 clean_single_select <- function(data)
 {
 q_plot <-
@@ -86,6 +85,7 @@ q_plot <-
 
 }
 
+# Plot single select data
 plot_single_select <- function(data)
 {
   r <- 
@@ -98,7 +98,7 @@ plot_single_select <- function(data)
   r
 }
 
-# Get all responses for single select question
+# Get all responses for single select questions that are sub questions of a larger question
 get_single_selection_2 <- function(data, q.name)
 {
   responses <- 
@@ -111,7 +111,7 @@ get_single_selection_2 <- function(data, q.name)
   return(responses)
 }
 
-
+# Clean responses for single select questions that are sub questions of a larger question
 clean_single_select_2 <- function(data)
 {
   q_plot <-
@@ -121,6 +121,66 @@ clean_single_select_2 <- function(data)
     mutate(percent_responded = round(100*(count / sum(count)), 2)) %>%
     arrange(desc(count))
   
+}
+
+
+# Get all responses for single select question with potential 0s as responses
+get_single_selection_0 <- function(data, q.name)
+{
+  responses <- 
+    data %>%
+    select(id = Respondent.ID, question = q.name) %>%
+    slice(2:nrow(.)) %>%
+    rename(response = question)
+  
+  return(responses)
+}
+
+# plot single select response.y
+plot_single_select_y <- function(data)
+{
+  r <- 
+    ggplot(data, aes(x = reorder(response.y, count), y = percent_responded)) +
+    geom_bar(stat = 'identity') +
+    coord_flip() +
+    geom_text(aes(label= paste(round(percent_responded, 0), count, sep = "%:"))) +
+    xlab("Response") +
+    ylab("% Responded, Count")
+  r
+}
+
+# Clean responses for single select questions that have responses.y
+clean_single_select_y <- function(data)
+{
+  q_plot <-
+    data %>%
+    group_by(response.y) %>%
+    summarize(count = n()) %>%
+    mutate(percent_responded = round(100*(count / sum(count)), 2)) %>%
+    arrange(desc(count))
+  
+}
+
+# Clean question data for plotting for choose all filtering with chosen.y
+clean_choose_all_y <- function(data)
+{
+  q_data %>%
+    group_by(chosen.y) %>%
+    summarize(count = n()) %>%
+    mutate(percent_responded = round(100*(count / q_n), 2)) %>%
+    arrange(desc(count))
+}
+
+# Plot the clean data for choose all filtering with chosen.y
+plot_choose_all_y <- function(data)
+{
+  r <- 
+    ggplot(data, aes(x = reorder(chosen.y, count), y = percent_responded)) +
+    geom_bar(stat = 'identity') +
+    coord_flip() +
+    geom_text(aes(label= paste(round(percent_responded, 0), count, sep = "%:"))) +
+    xlab("Response") +
+    ylab("% Responded, Count")
 }
 
 
